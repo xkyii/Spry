784e98d9401a1ee5301eb275d8cb2274b3e7635c
xkyii <xkyii@qq.com>
2023-01-20 00:10:38 +0800

    Up: 注册入参参数校验

--- spry-admin/src/main/java/com/xkyii/spry/admin/dto/login/RegisterInput.java
+++ spry-admin/src/main/java/com/xkyii/spry/admin/dto/login/RegisterInput.java
@@ -1,5 +1,9 @@
 package com.xkyii.spry.admin.dto.login;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 public class RegisterInput {
@@ -7,11 +11,14 @@ public class RegisterInput {
      * 用户名
      */
     @NotBlank(message = "用户名不能为空")
+    @Length(min=2, max=20, message = "用户名长度最小{min},最大{max}")
     private String username;
 
     /**
      * 用户密码
      */
+    @NotBlank(message = "用户密码不能为空")
+    @Length(min=5, max=20, message = "用户密码长度最小{min},最大{max}")
     private String password;
 
     /**
