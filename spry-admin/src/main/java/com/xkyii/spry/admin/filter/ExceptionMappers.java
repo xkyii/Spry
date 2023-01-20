db3e06004d6f02b341994a66c5f461f90b7b285d
xkyii <xkyii@qq.com>
2023-01-21 00:00:30 +0800

    Up: 处理校验Response(进行中)

--- spry-admin/src/main/java/com/xkyii/spry/admin/filter/ExceptionMappers.java
+++ spry-admin/src/main/java/com/xkyii/spry/admin/filter/ExceptionMappers.java
@@ -2,13 +2,25 @@ package com.xkyii.spry.admin.filter;
 
 
 import com.xkyii.spry.common.error.ApiException;
+import io.smallrye.mutiny.Uni;
 import org.hibernate.reactive.exception.ConstraintViolationException;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 import org.jboss.resteasy.reactive.RestResponse;
+import org.jboss.resteasy.reactive.server.ServerResponseFilter;
+
+import javax.ws.rs.container.ContainerResponseContext;
 
 class ExceptionMappers {
     @ServerExceptionMapper
     public RestResponse<String> mapException(ConstraintViolationException x) {
-        return RestResponse.status(RestResponse.Status.NOT_FOUND, "hello > " + x.getClass().getName());
+        return RestResponse.status(RestResponse.Status.NOT_FOUND, "mapped Exception > " + x.getClass().getName());
     }
+
+    @ServerResponseFilter
+    public void  mapResponse(ContainerResponseContext context) {
+        if ("true".equals(context.getHeaderString("validation-exception"))) {
+            context.setEntity("mapped Response >" + context.getEntity().getClass().getName());
+        }
+    }
+
 }
