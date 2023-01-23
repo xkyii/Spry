211c476b0e08a54c23f31e40c2370890576c405e
xkyii <xkyii@qq.com>
2023-01-24 00:16:43 +0800

    Up: 在Filter中填充Response.message

--- spry-admin/src/main/java/com/xkyii/spry/admin/filter/ExceptionMappers.java
+++ spry-admin/src/main/java/com/xkyii/spry/admin/filter/ExceptionMappers.java
@@ -1,14 +1,29 @@
 package com.xkyii.spry.admin.filter;
 
 
+import com.xkyii.spry.common.dto.Response;
+import com.xkyii.spry.common.error.ErrorMessageManager;
+import io.smallrye.mutiny.Uni;
+import io.vertx.core.json.Json;
 import org.hibernate.reactive.exception.ConstraintViolationException;
+import org.jboss.logging.Logger;
 import org.jboss.resteasy.reactive.RestResponse;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 import org.jboss.resteasy.reactive.server.ServerResponseFilter;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.ws.rs.container.ContainerResponseContext;
 
+@ApplicationScoped
 class ExceptionMappers {
+
+    @Inject
+    Logger logger;
+
+    @Inject
+    ErrorMessageManager emm;
+
     @ServerExceptionMapper
     public RestResponse<String> mapException(ConstraintViolationException x) {
         return RestResponse.status(RestResponse.Status.NOT_FOUND, "mapped Exception > " + x.getClass().getName());
@@ -16,8 +31,23 @@ class ExceptionMappers {
 
     @ServerResponseFilter
     public void  mapResponse(ContainerResponseContext context) {
+        logger.info("mapResponse");
+
+        // 参数校验异常
         if ("true".equals(context.getHeaderString("validation-exception"))) {
             context.setEntity("mapped Response >" + context.getEntity().getClass().getName());
+        }
+
+        if (context.hasEntity()) {
+            Object entity = context.getEntity();
+            // 对自定义Response填充message
+            if (entity instanceof Response) {
+                @SuppressWarnings("rawtypes") Response r = (Response) entity;
+                if (r.getMessage() == null) {
+                    r.setMessage(emm.getMessage(r.getCode()));
+                }
+                logger.info(Json.encodePrettily(entity));
+            }
         }
     }
 
--- spry-admin/src/main/java/com/xkyii/spry/admin/resource/SysUserResource.java
+++ spry-admin/src/main/java/com/xkyii/spry/admin/resource/SysUserResource.java
@@ -4,11 +4,7 @@ package com.xkyii.spry.admin.resource;
 import com.xkyii.spry.admin.dto.login.RegisterInput;
 import com.xkyii.spry.admin.entity.SysUser;
 import com.xkyii.spry.admin.service.ISysUserService;
-import com.xkyii.spry.common.config.SpryConfig;
 import com.xkyii.spry.common.dto.Response;
-import com.xkyii.spry.common.error.ApiException;
-import com.xkyii.spry.common.error.ErrorMessageManager;
-import com.xkyii.spry.common.resource.AResource;
 import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional;
 import io.smallrye.mutiny.Uni;
 import io.vertx.core.json.Json;
@@ -16,13 +12,16 @@ import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/user")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-public class SysUserResource extends AResource {
+public class SysUserResource {
 
     @Inject
     Logger logger;
@@ -30,19 +29,14 @@ public class SysUserResource extends AResource {
     @Inject
     ISysUserService userService;
 
-    protected SysUserResource(ErrorMessageManager emm) {
-        super(emm);
-    }
-
     @POST
     @Path("register")
     @ReactiveTransactional
     public Uni<Response<SysUser>> register(@Valid RegisterInput input) {
         logger.infof("注册用户,入参: \n%s", Json.encodePrettily(input));
 
-        throw new ApiException(0, "fff");
-//        return userService.register(input)
-//                .onItem().transform(super::ok);
+        return userService.register(input)
+                .onItem().transform(Response::ok);
     }
 
 }
