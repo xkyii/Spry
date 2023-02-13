62ba70c756877cccf368151efe0f757835b7ddd4
xkyii <xkyii@qq.com>
2023-02-13 11:12:24 +0800

    Add: 权限

--- spry-admin/src/main/java/com/xkyii/spry/admin/resource/EntryResource.java
+++ spry-admin/src/main/java/com/xkyii/spry/admin/resource/EntryResource.java
@@ -1,12 +1,23 @@
 package com.xkyii.spry.admin.resource;
 
 import com.xkyii.spry.common.config.SpryConfig;
+import io.quarkus.security.Authenticated;
+import jakarta.annotation.security.DenyAll;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
 
 
 @Path("")
+@Consumes(MediaType.TEXT_PLAIN)
+@Produces(MediaType.TEXT_PLAIN)
 public class EntryResource {
 
     @Inject
@@ -15,5 +26,33 @@ public class EntryResource {
     @GET
     public String index() {
         return String.format("欢迎使用%s, 当前版本: v%s @%s", config.name(), config.version(), config.copyrightYear());
+    }
+
+    @GET
+    @Path("test/permitall")
+    @PermitAll
+    public String permitall(@Context SecurityContext context) {
+        return "permitall";
+    }
+
+    @GET
+    @Path("test/denyall")
+    @DenyAll
+    public String denyall(@Context SecurityContext context) {
+        return "denyall";
+    }
+
+    @GET
+    @Path("test/authenticated")
+    @Authenticated
+    public String authenticated(@Context SecurityContext context) {
+        return "authenticated";
+    }
+
+    @GET
+    @Path("test/rolesallowed")
+    @RolesAllowed("Admin")
+    public String rolesallowed(@Context SecurityContext context) {
+        return "rolesallowed";
     }
 }
--- spry-admin/src/main/java/com/xkyii/spry/admin/service/impl/TokenService.java
+++ spry-admin/src/main/java/com/xkyii/spry/admin/service/impl/TokenService.java
@@ -2,7 +2,9 @@ package com.xkyii.spry.admin.service.impl;
 
 import com.xkyii.spry.admin.entity.SysUser;
 import com.xkyii.spry.admin.service.ITokenService;
+import io.quarkus.runtime.util.StringUtil;
 import io.smallrye.jwt.build.Jwt;
+import io.smallrye.jwt.build.JwtClaimsBuilder;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.Arrays;
@@ -13,10 +15,13 @@ public class TokenService implements ITokenService {
 
     @Override
     public String generateToken(SysUser user) {
-        return Jwt.claims()
-                .issuer("https://xkyii.com/issuer")
-                .groups(new HashSet<>(Arrays.asList("User", "Admin")))
-                .expiresIn(86400)
-                .sign();
+        JwtClaimsBuilder claims = Jwt.claims();
+        claims.issuer("https://xkyii.com/issuer");
+        claims.upn(StringUtil.isNullOrEmpty(user.getEmail())
+            ? (user.getUserName() + "@xkyii.com")
+            : user.getEmail());
+        claims.groups(new HashSet<>(Arrays.asList("User", "Admin")));
+        claims.expiresIn(86400);
+        return claims.sign();
     }
 }
