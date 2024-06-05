795ad1436a66606140fbba9a32238324b907168b
xkyii@th <xkyii@foxmail.com>
2024-06-05 13:36:47 +0800

    Up

--- spry-quarkus/src/main/java/com/xkyii/spry/resource/gen/UserResource.java
+++ spry-quarkus/src/main/java/com/xkyii/spry/resource/gen/UserResource.java
@@ -1,10 +1,8 @@
-package com.xkyii.spry.resource;
+package com.xkyii.spry.resource.gen;
 
 import com.xkyii.spry.entity.User;
 import com.xkyii.spry.repository.UserRepository;
 import io.quarkus.hibernate.orm.rest.data.panache.PanacheRepositoryResource;
-import io.quarkus.rest.data.panache.ResourceProperties;
 
-@ResourceProperties(hal = true)
 public interface UserResource extends PanacheRepositoryResource<UserRepository, User, Long> {
 }
