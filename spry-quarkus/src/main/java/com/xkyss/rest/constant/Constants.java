7ce810799c56b4e72dd8f13a00bbeef510a0dba2
xkyii <xkyii@foxmail.com>
2024-06-10 21:33:30 +0800

    Add: BuildConfig

---
+++ spry-quarkus/src/main/java/com/xkyss/rest/constant/Constants.java
@@ -0,0 +1,7 @@
+package com.xkyss.rest.constant;
+
+public class Constants {
+    public static final String CONFIG_PREFIX = "xkyss";
+    public static final String CONFIG_REST_PREFIX = CONFIG_PREFIX + ".rest";
+    public static final String CONFIG_REST_BUILD_PREFIX = CONFIG_REST_PREFIX + ".build";
+}
--- spry-quarkus/src/main/java/com/xkyss/rest/filter/ResponseFilter.java
+++ spry-quarkus/src/main/java/com/xkyss/rest/filter/ResponseFilter.java
@@ -1,13 +1,20 @@
 package com.xkyss.rest.filter;
 
+import com.xkyss.rest.config.BuildConfig;
 import com.xkyss.rest.dto.Response;
+import io.quarkus.arc.properties.IfBuildProperty;
 import io.vertx.core.http.HttpServerResponse;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.ext.Provider;
 import org.jboss.resteasy.reactive.server.ServerResponseFilter;
 
 @Provider
+@IfBuildProperty(name = "xkyss.rest.build.response-filter.enabled", stringValue = "true")
 public class ResponseFilter {
+
+    @Inject
+    BuildConfig buildConfig;
 
     @ServerResponseFilter
     public void mapResponse(ContainerResponseContext response, HttpServerResponse resp) {
