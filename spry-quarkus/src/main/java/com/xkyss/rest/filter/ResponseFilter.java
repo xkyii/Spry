1784234f1bd529e8e36a8cd3a93abffb299d3c43
xkyii@th <xkyii@foxmail.com>
2024-06-11 15:31:27 +0800

    Up: 一些基本配置

--- spry-quarkus/src/main/java/com/xkyss/rest/filter/ResponseFilter.java
+++ spry-quarkus/src/main/java/com/xkyss/rest/filter/ResponseFilter.java
@@ -13,7 +13,6 @@ import org.jboss.resteasy.reactive.server.ServerResponseFilter;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Objects;
 
 @ApplicationScoped
 @IfBuildProperty(name = "xkyss.build.rest.response-filter.enabled", stringValue = "true")
