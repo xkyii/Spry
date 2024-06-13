e321d5de805e8b7953fc8b19eeeb08dfc5f3b235
xkyii <xkyii@foxmail.com>
2024-06-13 21:17:24 +0800

    Add: HttpLogFilter

--- spry-quarkus/src/main/java/com/xkyss/rest/constant/Constants.java
+++ spry-quarkus/src/main/java/com/xkyss/rest/constant/Constants.java
@@ -5,4 +5,15 @@ public class Constants {
     public static final String CONFIG_BUILD_PREFIX = "xkyss.build";
     public static final String CONFIG_REST_PREFIX = CONFIG_PREFIX + ".rest";
     public static final String CONFIG_REST_BUILD_PREFIX = CONFIG_BUILD_PREFIX + ".rest";
+
+
+    public static final String KEY_REQUEST_TIME = "request-time";
+    public static final String KEY_REQUEST_BODY = "request-body";
+    public static final String KEY_REQUEST_BODY_HASHCODE = "request-body-hashcode-key";
+    public static final String KEY_RESPONSE_BODY = "response-body";
+
+
+    /// 优先级 (值越小,优先级越高)
+    public static final int PRIORITY_REST = 500;
+    public static final int PRIORITY_REST_HTTP_LOG = PRIORITY_REST + 1;
 }
---
+++ spry-quarkus/src/main/java/com/xkyss/rest/filter/HttpLogFilter.java
@@ -0,0 +1,177 @@
+package com.xkyss.rest.filter;
+
+import com.xkyss.rest.config.RuntimeConfig;
+import io.quarkus.runtime.util.StringUtil;
+import io.vertx.core.Handler;
+import io.vertx.core.MultiMap;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.core.MediaType;
+import org.apache.commons.io.IOUtils;
+import org.jboss.logging.Logger;
+import org.jboss.resteasy.reactive.server.ServerRequestFilter;
+import org.jboss.resteasy.reactive.server.ServerResponseFilter;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.StringJoiner;
+
+import static com.xkyss.rest.constant.Constants.*;
+
+@ApplicationScoped
+public class HttpLogFilter {
+    @Inject
+    Logger logger;
+
+    @Inject
+    RuntimeConfig config;
+
+    @Inject
+    Vertx vertx;
+
+    private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
+
+    @ServerRequestFilter
+    public void mapRequest(ContainerRequestContext request, RoutingContext rc) {
+        vertx.executeBlocking(() -> {
+            try {
+                boolean isLogBody = request.hasEntity();
+                if (isLogBody) {
+                    String text = IOUtils.toString(request.getEntityStream(), StandardCharsets.UTF_8);
+                    request.setEntityStream(IOUtils.toInputStream(text, StandardCharsets.UTF_8));
+                    rc.put(KEY_REQUEST_BODY_HASHCODE, text.hashCode());
+
+                    if ((request.getMediaType().isCompatible(MediaType.APPLICATION_JSON_TYPE))) {
+                        rc.put(KEY_REQUEST_BODY, Json.encodePrettily(Json.decodeValue(text)));
+                    }
+                    else {
+                        rc.put(KEY_REQUEST_BODY, text);
+                    }
+                    return true;
+                }
+                else {
+                    rc.put(KEY_REQUEST_BODY, "<EMPTY>");
+                    return false;
+                }
+            } catch (Exception e) {
+                logger.warn("获取Request Body失败", e);
+                rc.put(KEY_REQUEST_BODY, "<EXCEPTION>");
+                return false;
+            }
+        }, true);
+    }
+
+
+    @ServerResponseFilter(priority = PRIORITY_REST_HTTP_LOG)
+    public void mapResponse(ContainerResponseContext response, RoutingContext rc) {
+        vertx.executeBlocking(() -> {
+            try {
+                boolean isLogBody = response.hasEntity();
+                if (isLogBody) {
+                    Object entity = response.getEntity();
+                    if (entity instanceof String) {
+                        rc.put(KEY_RESPONSE_BODY, entity);
+                    }
+                    else if (entity instanceof byte[]) {
+                        rc.put(KEY_RESPONSE_BODY, Json.encodePrettily(Json.decodeValue(Buffer.buffer((byte[]) entity))));
+                    }
+                    else {
+                        rc.put(KEY_RESPONSE_BODY, Json.encodePrettily(entity));
+                    }
+                } else {
+                    rc.put(KEY_RESPONSE_BODY, "<EMPTY>");
+                }
+                return true;
+            } catch (Exception e) {
+                logger.warn("获取Response Body失败", e);
+                rc.put(KEY_RESPONSE_BODY, "<EXCEPTION>");
+                return false;
+            }
+        }, true);
+    }
+
+    void register(@Observes Router router) {
+        LoggerHandler loggerHandler = new LoggerHandler(logger);
+
+        router.route().order(Integer.MIN_VALUE).handler(loggerHandler);
+
+        logger.info("已注册Http日志过滤.");
+    }
+
+    public static class LoggerHandler implements Handler<RoutingContext> {
+        private final Logger logger;
+
+        public LoggerHandler(Logger logger) {
+            this.logger = logger;
+        }
+
+        @Override
+        public void handle(RoutingContext rc) {
+            rc.put(KEY_REQUEST_TIME, new Date());
+            rc.put(KEY_REQUEST_BODY, "<EMPTY0>");
+            rc.put(KEY_RESPONSE_BODY, "<EMPTY0>");
+
+            rc.addEndHandler().onComplete(_ -> {
+                StringBuilder sb = new StringBuilder();
+                Date requestAt = rc.get(KEY_REQUEST_TIME);
+
+                // request
+                {
+                    HttpServerRequest request = rc.request();
+                    sb.append("\n\n[Request]:\n");
+                    sb.append("[Request At]: ").append(dateFormat.format(requestAt)).append("\n\n");
+                    sb.append(request.method()).append(" ").append(request.uri()).append(" ").append(request.version().alpnName()).append("\n");
+                    String text = readAttribute(request.headers());
+                    sb.append(StringUtil.isNullOrEmpty(text) ? "<Request head is empty>" : text).append("\n\n");
+                    sb.append((String) rc.get(KEY_REQUEST_BODY));
+                }
+
+                // response
+                {
+                    HttpServerResponse response = rc.response();
+                    Date responseAt = new Date();
+                    sb.append("\n\n\n[Response]:\n");
+                    sb.append("[ Request At]: ").append(dateFormat.format(requestAt)).append("\n");
+                    sb.append("[Response At]: ").append(dateFormat.format(responseAt)).append("\n");
+                    sb.append("[ Total Cast]: ").append(responseAt.getTime() - requestAt.getTime()).append(" ms\n\n");
+                    sb.append("Status: ").append(response.getStatusCode()).append(", ").append(response.getStatusMessage()).append("\n");
+                    String text = readAttribute(response.headers());
+                    sb.append(StringUtil.isNullOrEmpty(text) ? "<Response head is empty>" : text).append("\n\n");
+                    sb.append((String) rc.get(KEY_RESPONSE_BODY));
+                }
+
+                logger.info(sb);
+            });
+            rc.next();
+        }
+
+        private static String readAttribute(MultiMap headers) {
+            if (headers.isEmpty()) {
+                return null;
+            }
+            else {
+                final StringJoiner joiner = new StringJoiner(System.lineSeparator());
+
+                for (Map.Entry<String, String> header : headers) {
+                    joiner.add(header.getKey() + ": " + header.getValue());
+                }
+
+                return joiner.toString();
+            }
+        }
+    }
+}
