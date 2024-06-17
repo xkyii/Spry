dcafd427cfb71de11658d4d9102b6f4dd006ffbb
xkyii@th <xkyii@foxmail.com>
2024-06-17 16:53:45 +0800

    Up: list 分页

--- spry-refine/src/providers/data-provider/index.ts
+++ spry-refine/src/providers/data-provider/index.ts
@@ -2,14 +2,28 @@
 
 import { DataProvider, GetOneResponse, CreateResponse, UpdateResponse, DeleteOneResponse } from "@refinedev/core";
 import axios from "axios";
+import { stringify } from "query-string";
 
 const API_URL = "http://localhost:8080/api";
-const http = axios.create({ baseURL: API_URL, timeout: 2000});
+const http = axios.create({ baseURL: API_URL, timeout: 2000 });
 
 export const dataProvider: DataProvider = {
     getApiUrl: () => API_URL,
     getList: async ({ resource, pagination, sorters, filters, meta }) => {
-        const response = await http.get("/user");
+
+        const { current = 1, pageSize = 10, mode = "server" } = pagination ?? {};
+
+        const query: {
+            page?: number;
+            size?: number;
+        } = {};
+
+        if (mode === "server") {
+            query.page = current - 1;
+            query.size = pageSize;
+        }
+
+        const response = await http.get(`${resource}?${stringify(query)}`);
         const data = response.data.data;
         return {
             data: data,
@@ -37,3 +51,4 @@ export const dataProvider: DataProvider = {
         } as DeleteOneResponse<any>;
     },
 };
+
