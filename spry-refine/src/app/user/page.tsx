e5fcf744db2b6286677dc0b17655e82587e7c8a0
xkyii@th <xkyii@foxmail.com>
2024-06-26 11:34:19 +0800

    Add: mui/x-tree-view

--- spry-refine/src/app/user/page.tsx
+++ spry-refine/src/app/user/page.tsx
@@ -8,7 +8,41 @@ import {
   ShowButton,
   useDataGrid,
 } from "@refinedev/mui";
+import Grid from '@mui/material/Grid';
+import { TreeViewBaseItem } from '@mui/x-tree-view/models';
+import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
 import React from "react";
+
+
+const depts: TreeViewBaseItem[] = [
+  {
+    id: 'grid',
+    label: 'Data Grid',
+    children: [
+      { id: 'grid-community', label: '@mui/x-data-grid' },
+      { id: 'grid-pro', label: '@mui/x-data-grid-pro' },
+      { id: 'grid-premium', label: '@mui/x-data-grid-premium' },
+    ],
+  },
+  {
+    id: 'pickers',
+    label: 'Date and Time Pickers',
+    children: [
+      { id: 'pickers-community', label: '@mui/x-date-pickers' },
+      { id: 'pickers-pro', label: '@mui/x-date-pickers-pro' },
+    ],
+  },
+  {
+    id: 'charts',
+    label: 'Charts',
+    children: [{ id: 'charts-community', label: '@mui/x-charts' }],
+  },
+  {
+    id: 'tree-view',
+    label: 'Tree View',
+    children: [{ id: 'tree-view-community', label: '@mui/x-tree-view' }],
+  },
+];
 
 export default function UserList() {
   const { dataGridProps } = useDataGrid({});
@@ -56,7 +90,15 @@ export default function UserList() {
 
   return (
     <List>
-      <DataGrid {...dataGridProps} columns={columns} autoHeight />
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <RichTreeView items={depts} />
+        </Grid>
+        <Grid item xs={9}>
+          <DataGrid {...dataGridProps} columns={columns} autoHeight />
+        </Grid>
+      </Grid>
+
     </List>
   );
 }
