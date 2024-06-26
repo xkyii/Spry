6d151f070ce05f188194d8496d08ae2e6e186734
xkyii@th <xkyii@foxmail.com>
2024-06-26 11:08:53 +0800

    Del: 移除样例代码

--- spry-refine/src/app/layout.tsx
+++ spry-refine/src/app/layout.tsx
@@ -47,30 +47,6 @@ export default function RootLayout({
                     notificationProvider={useNotificationProvider}
                     authProvider={authProvider}
                     resources={[
-                      // {
-                      //   name: "blog_posts",
-                      //   list: "/blog-posts",
-                      //   create: "/blog-posts/create",
-                      //   edit: "/blog-posts/edit/:id",
-                      //   show: "/blog-posts/show/:id",
-                      //   meta: {
-                      //     canDelete: true,
-                      //     label: "Blog Posts",
-                      //     dataProviderName: "fake",
-                      //   },
-                      // },
-                      // {
-                      //   name: "categories",
-                      //   list: "/categories",
-                      //   create: "/categories/create",
-                      //   edit: "/categories/edit/:id",
-                      //   show: "/categories/show/:id",
-                      //   meta: {
-                      //     canDelete: true,
-                      //     label: "Categories",
-                      //     dataProviderName: "fake",
-                      //   },
-                      // },
                       {
                         name: "dept",
                         list: "/dept",
